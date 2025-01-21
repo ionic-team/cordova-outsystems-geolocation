3f613c02b2772e3bc2e4408ed0fba20ea2ae4407
Alexandre Jacinto <alexandrejacinto@Alexandres-MacBook-Pro.local>
2025-01-21 12:12:55 +0000

    chore: update dependency to IONGeolocationLib-iOS (verify)

---
+++ packages/cordova-plugin/ios/IONGLOCConfigurationModel+Accuracy.swift
@@ -0,0 +1,14 @@
+import IONGeolocationLib
+
+extension IONGLOCConfigurationModel {
+    static func createWithAccuracy(_ isHighAccuracyEnabled: Bool) -> IONGLOCConfigurationModel {
+        let minimumDistance = isHighAccuracyEnabled ?
+            Constants.MinimumDistance.highAccuracy :
+            Constants.MinimumDistance.lowAccuracy
+
+        return .init(
+            enableHighAccuracy: isHighAccuracyEnabled,
+            minimumUpdateDistanceInMeters: minimumDistance
+        )
+    }
+}
---
+++ packages/cordova-plugin/ios/IONGLOCPositionModel+JSONTransformer.swift
@@ -0,0 +1,16 @@
+import IONGeolocationLib
+
+extension IONGLOCPositionModel {
+    func toResultDictionary() -> [String: Double] {
+        [
+            Constants.Position.altitude: altitude,
+            Constants.Position.heading: course,
+            Constants.Position.accuracy: horizontalAccuracy,
+            Constants.Position.latitude: latitude,
+            Constants.Position.longitude: longitude,
+            Constants.Position.speed: speed,
+            Constants.Position.timestamp: timestamp,
+            Constants.Position.altitudeAccuracy: verticalAccuracy
+        ]
+    }
+}
