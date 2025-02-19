853c6ceb55001e6e8bfc999f5e3291d47ebaef3a
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2025-02-19 17:09:33 +0000

    fix: Outsystems MABS 12 compatibility issues  (#7)
    * chore: Update checks for synapse
    
    * chore: update to new synapse version
    
    * fix: plugin.xml declaration
    
    So that MABS 12 installs this plugin as a cordova plugin when running "npx cap sync"
    
    * fix: properties in root package.json
    
    Without this, MABS 12 cannot install plugin using "npm pack".
    
    * chore: Update CHANGELOG.md
    
    * chore: Update synapse to stable version

--- packages/cordova-plugin/dist/plugin.js
+++ packages/cordova-plugin/dist/plugin.js
@@ -44,8 +44,8 @@
       }
     );
   }
-  function y() {
-    window.CapacitorUtils = window.CapacitorUtils || {}, window.Capacitor !== void 0 ? s(window) : window.cordova !== void 0 && u(window);
+  function y(t = false) {
+    window.CapacitorUtils = window.CapacitorUtils || {}, window.Capacitor !== void 0 && !t ? s(window) : window.cordova !== void 0 && u(window);
   }
   const CurrentPositionOptionsDefault = {
     enableHighAccuracy: false,
@@ -108,5 +108,5 @@
     watchPosition,
     clearWatch
   };
-  y();
+  y(true);
 });
