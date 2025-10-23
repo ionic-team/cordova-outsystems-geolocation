32327d1f9deee6940fc7ecc42c985fc65c344cb2
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-10-23 10:32:19 +0100

    fix: outsystems-wrapper build (again)

--- packages/outsystems-wrapper/dist/outsystems.js
+++ packages/outsystems-wrapper/dist/outsystems.js
@@ -32,14 +32,11 @@ var __privateMethod = (obj, member, method) => (__accessCheck(obj, member, "acce
   const randomUUID = typeof crypto !== "undefined" && crypto.randomUUID && crypto.randomUUID.bind(crypto);
   const native = { randomUUID };
   function v4(options, buf, offset) {
-    if (native.randomUUID && true && !options) {
+    if (native.randomUUID && !buf && !options) {
       return native.randomUUID();
     }
     options = options || {};
-    const rnds = options.random ?? options.rng?.() ?? rng();
-    if (rnds.length < 16) {
-      throw new Error("Random bytes length must be >= 16");
-    }
+    const rnds = options.random || (options.rng || rng)();
     rnds[6] = rnds[6] & 15 | 64;
     rnds[8] = rnds[8] & 63 | 128;
     return unsafeStringify(rnds);
