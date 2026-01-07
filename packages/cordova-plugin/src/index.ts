5e9ab93710baab1df079e2dad13de17d01a94f6a
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-01-07 17:36:21 +0000

    chore: remove Synapse dependency from plugin

--- packages/cordova-plugin/src/index.ts
+++ packages/cordova-plugin/src/index.ts
@@ -1,7 +1,2 @@
-import { exposeSynapse } from '@capacitor/synapse';
-
-// true -> to make sure Synapse uses this cordova plugin for capacitor builds.
-exposeSynapse(true);
-
 export * from './web';
 export * from './definitions';
