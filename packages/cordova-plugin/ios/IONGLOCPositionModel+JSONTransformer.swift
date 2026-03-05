026e56a638340ec2197c16dc611e77814a8554a5
Chace Daniels <chace.daniels@outsystems.com>
2026-03-05 09:32:45 -0600

    remove local binaries

--- packages/cordova-plugin/ios/IONGLOCPositionModel+JSONTransformer.swift
+++ packages/cordova-plugin/ios/IONGLOCPositionModel+JSONTransformer.swift
@@ -2,14 +2,14 @@ import IONGeolocationLib
 
 extension IONGLOCPositionModel {
     func toResultDictionary() -> [String: Any] {
-        let headingValue = trueHeading != -1.0 ? trueHeading : (magneticHeading != -1.0 ? magneticHeading : course)
+        let headingValue = trueHeading ?? magneticHeading ?? (course != -1.0 ? course : nil)
         return [
             Constants.Position.altitude: altitude,
-            Constants.Position.heading: headingValue != -1.0 ? headingValue : NSNull(),
-            Constants.Position.magneticHeading: magneticHeading != -1.0 ? magneticHeading : NSNull(),
-            Constants.Position.trueHeading: trueHeading != -1.0 ? trueHeading : NSNull(),
-            Constants.Position.headingAccuracy: headingAccuracy != -1.0 ? headingAccuracy : NSNull(),
-            Constants.Position.course: course,
+            Constants.Position.heading: headingValue ?? NSNull(),
+            Constants.Position.magneticHeading: magneticHeading ?? NSNull(),
+            Constants.Position.trueHeading: trueHeading ?? NSNull(),
+            Constants.Position.headingAccuracy: headingAccuracy ?? NSNull(),
+            Constants.Position.course: course != -1.0 ? course : NSNull(),
             Constants.Position.accuracy: horizontalAccuracy,
             Constants.Position.latitude: latitude,
             Constants.Position.longitude: longitude,
